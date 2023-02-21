65dd5972453b991c58f59a2c2d2f0ecf5f25ed3f
jiameng <17737812427@163.com>
2023-02-21 10:11:54 +0800

    feat:提交第一版

---
+++ Example/JMIMKit/JMTest.swift
@@ -0,0 +1,9 @@
+//
+//  JMTest.swift
+//  JMIMKit
+//
+//  Created by JiaMeng on 2023/2/20.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import Foundation
---
+++ JMIMKit/Classes/JMTest.swift
@@ -0,0 +1,19 @@
+//
+//  JMTest.swift
+//  JMIMKit
+//
+//  Created by JiaMeng on 2023/2/20.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+open class JMTest {
+    // 如果需要外部模块初始化JMTest类,需要手动添加public init方法
+    public init() {
+//        self.key= key
+    }
+    open func run() {
+        print("正在测试JMTest.run()函数")
+    }
+}
